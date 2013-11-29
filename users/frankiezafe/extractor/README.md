1568056830bfe6c6eca8bc5a8986859bf906238d
frankiezafe <frankiezafe@shirogauna.(none)>
2013-11-29 17:11:49 +0100

    working code, preparing a demo with suzanne.blend

--- users/frankiezafe/extractor/README.md
+++ users/frankiezafe/extractor/README.md
@@ -11,3 +11,4 @@ doc:
 http://www.blender.org/documentation/blender_python_api_2_67_1/bpy.types.Text.html#bpy.types.Text
 http://www.blender.org/documentation/blender_python_api_2_61_0/info_tips_and_tricks.html
 http://wiki.blender.org/index.php/User:Ideasman42/BlenderAsPyModule
+http://www.blender.org/documentation/blender_python_api_2_61_0/bpy.ops.export_scene.html
--- users/frankiezafe/extractor/extractor.py
+++ users/frankiezafe/extractor/extractor.py
@@ -3,10 +3,18 @@ import os
 
 folder = bpy.path.abspath( "//" )
 
-# creating subfolder for texts
+# creating subfolder for texts and objects
 foldertext = folder+ "texts/"
 if not os.path.exists(foldertext):
     os.makedirs(foldertext)
+
+folderscenes = folder+ "scenes/"
+if not os.path.exists(folderscenes):
+    os.makedirs(folderscenes)
+
+folderobjects = folder+ "objects/"
+if not os.path.exists(folderobjects):
+    os.makedirs(folderobjects)
 
 # extracting all text blocks found in .blend
 for t in range( 0, len( bpy.data.texts ) ):
@@ -14,6 +22,18 @@ for t in range( 0, len( bpy.data.texts ) ):
 	f = open( foldertext + ct.name, 'w' )
 	f.write( ct.as_string() )
 	f.close()
+
+for s in bpy.data.scenes:
+	bpy.context.screen.scene = s
+	bpy.ops.export_scene.obj( filepath = folderscenes + s.name + ".obj", path_mode = "STRIP" )
+	#print( s.name )
+
+for o in bpy.data.objects:
+	if o.type == "MESH":
+		o.select = True
+		bpy.ops.export_mesh.stl( filepath = folderobjects + o.name + ".stl" )
+		o.select = False
+		#print( o.name, ">", o.type )
 
 # extracting all packed files
 bpy.ops.file.unpack_all( method = "USE_LOCAL" )
