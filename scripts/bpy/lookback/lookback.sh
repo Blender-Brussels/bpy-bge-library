e42bd92b8f531e3f0560c3934a129d991c798a6b
juego <juego@requiem4tv.com>
2014-03-08 18:09:24 +0100

    A better bash

--- scripts/bpy/lookback/lookback.sh
+++ scripts/bpy/lookback/lookback.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 # If Blender is in your PATH
-blender -b -P lookback.py
+#blender -b -P lookback.py
 
-# If blender is not in your path
+# If blender is NOT in your path
 # /absolute/path/to/blender/folder/./blender -b -P lookback.py
+# Example
+/home/juego/blender-2.68a-linux-glibc211-i686/./blender -b -P lookback.py
 
-# You need to have imagemageck installed
-convert output/* lookback.gif 
+
+# You need to have imagemagick installed
+convert output/* lookback.gif
+
+# Removes temporary output folder
+rm -R output/
+
+echo "Done. Have a look at lookback.gif"
